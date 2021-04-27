b31856be3ac703ac5f2d063a58b219dced3e21f0
sandi3ai <sandi.podrzaj@gmail.com>
2021-04-27 16:08:30 +0200

    nice example of fetching and mapping data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React /*useState*/ from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loading: true,
+    people: [],
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const url = "https://api.randomuser.me/?results=5";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    this.setState({ people: data.results });
+  }
+
+  render() {
+    return (
+      <div className="data">
+        {this.state.loading & !this.state.people.length ? (
+          <div>loading...</div>
+        ) : (
+          <div>
+            {this.state.people.map((person, i) => (
+              <div className="mappingPeople" key={i}>
+                <div>Name: {person.name.first}</div>
+                <div>Lastname: {person.name.last}</div>
+                <div>State: {person.location.state}</div>
+                <div>e-mail: {person.email}</div>
+                <div>
+                  <img src={person.picture.large} alt="" />
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
